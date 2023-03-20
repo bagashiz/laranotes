a2bc1bd953bf1ebe296f9b2e1f2d188f3ed471e0
bagashiz <work.bitsy@simplelogin.com>
2023-03-20 18:11:56 +0700

    feat: integrate back-end logic to note related page

--- resources/views/notes/manage.blade.php
+++ resources/views/notes/manage.blade.php
@@ -9,26 +9,40 @@
 
             <table class="w-full table-auto rounded-sm">
                 <tbody>
+                @unless($notes->isEmpty())
+                    @foreach ($notes as $note)
+                        <tr class="border-gray-300">
+                            <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                                <a href="show.html">
+                                    {{ $note->title }}
+                                </a>
+                            </td>
+                            <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                                <a href="/notes/{{ $note->id }}/edit" class="text-blue-400 px-6 py-2 rounded-xl"><i
+                                        class="fa-solid fa-pen-to-square"></i>
+                                    Edit</a>
+                            </td>
+                            <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                                <form method="POST" action="/notes/{{ $note->id }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="text-red-600">
+                                        <i class="fa-solid fa-trash-can"></i>
+                                        Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                @else
                     <tr class="border-gray-300">
-                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                            <a href="/notes">
-                                Title
-                            </a>
-                        </td>
-                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                            <a href="/notes/edit" class="text-blue-400 px-6 py-2 rounded-xl"><i
-                                    class="fa-solid fa-pen-to-square"></i>
-                                Edit</a>
-                        </td>
-                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                            <form method="POST" action="/notes">
-                                <button class="text-red-600">
-                                    <i class="fa-solid fa-trash-can"></i>
-                                    Delete
-                                </button>
-                            </form>
+                        <td class="px-4 py-8 border-top border-bottom border-gray-300 text-lg">
+                            <p class="text-center">
+                                No Notes Found
+                            </p>
                         </td>
                     </tr>
+                @endunless
                 </tbody>
             </table>
         </x-card>
