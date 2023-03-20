23a7c728d1a1168f16ab52ee96786f4cd391a8f3
bagashiz <work.bitsy@simplelogin.com>
2023-03-20 18:11:18 +0700

    feat: integrate back-end logic to homepage

--- resources/views/components/note-card.blade.php
+++ resources/views/components/note-card.blade.php
@@ -3,10 +3,10 @@
 <x-card>
     <div class="flex">
         <div>
-            <h3 class="text-2xl">
+            <h3 class="text-2xl font-bold">
                 <a href="/notes/{{ $note->id }}">{{ $note->title }}</a>
             </h3>
-            <div class="text-xl font-bold mb-4">{{ $note->subtitle }}</div>
+            <div class="text-xl mb-4">{{ $note->subtitle }}</div>
             <x-note-tags :tags="$note->tags" />
         </div>
     </div>
